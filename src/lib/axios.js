cd9e917f928a3324c04cac7489af1b96d7bb1ace
Rafiu Islam <rafiuislam123@gmail.com>
2025-12-22 01:31:53 +0600

    commit for deployement

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
-  timeout: 10000,
+  baseURL: "https://school-dashboard-db.onrender.com", // incase of local dev use: "http://localhost:4000"
+  timeout: 15000,
 });
+
+// Use Render backend URL in production
+// const baseURL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://school-dashboard-db.onrender.com"
+//     : "http://localhost:4000";
+
+// const api = axios.create({
+//   baseURL,
+//   timeout: 15000,
+// });
 
 export default api;
